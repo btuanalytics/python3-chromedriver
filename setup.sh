e7b16c0e22ca6fe12e59f31ce63e3bc58c85caa8
crice <crice@btuanalytics.com>
2017-10-23 13:31:44 -0600

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash .\Miniconda3-latest-Linux-x86_64.sh
+
+apt-get -y update
+apt-get install -y wget
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get -y update
+apt-get install -y google-chrome-stable
+apt-get install -y apt-transport-https
+
+apt-get install -yqq unzip
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+ENV DISPLAY=:99
+
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/8/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql
